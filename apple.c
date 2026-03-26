149a00249f7235fe4bf245dd377538f45cb7dd01
ChilsungCider <22100487@handong.ac.kr>
2026-03-27 04:02:55 +0900

    create apple.c

---
+++ apple.c
@@ -0,0 +1,15 @@
+// 배고픈 모험가의 사과 수학
+
+/*
+* [Full Name] 이경민
+* [Student ID]: 22100487
+* [Honor Code Pledge]: 나 이경민은 하나님과 사람 앞에서 정직하고 성실하게 테스트를 수행하겠습니다.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    int apple[100];
+    
+}
